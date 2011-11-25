6721941d2f71e071e6d43c26c704163cb54df1c9
Mike Dory <mike@dory.me>
2011-11-25 12:37:44 -0500

    THERE I FIXED IT

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class MainHandler(BaseHandler):
 		else:
 			google_analytics_id = False
 
-		grab_all_the_things()
+		tweet_grabber.grab_all_the_things()
 
 		# oh hai redis
 		db = self.get_redis_conn()
