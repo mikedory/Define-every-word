e30659154bea7da8470907fb4f8932ad2c8f88b1
Mike Dory <mike@dory.me>
2011-11-25 16:37:13 -0500

    adapting for proper local use

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ from tornado.options import define, options
 define("port", default=5000, help="run on the given port", type=int)
 define("redis_host", help="hostname for redis", default="localhost", type=str) # set to localhost for local use
 define("redis_port", help="port number for redis", default=6379, type=int) # set to 6379 for local use
-define("redis_pass", help="password number for redis", default=None, type=int) # set to None for local use
-
+define("redis_pass", help="password for redis", default=None, type=int) # set to None for local use
+define("redis_db", help="default redis db number", default=0, type=int) # set to whatever for local use
+define("watched_bot", help="the name of the bot to watch", default=None)
 
 # application settings and handle mapping info
 class Application(tornado.web.Application):
@@ -42,9 +43,11 @@ class Application(tornado.web.Application):
 
 class BaseHandler(tornado.web.RequestHandler):
 	def get_redis_conn(self):
-		url = urlparse(os.environ.get('REDISTOGO_URL'))
-		return redis.Redis(host=url.hostname, port=url.port, password=url.password)
-
+		if os.environ.has_key('REDISTOGO_URL'):
+			url = urlparse(os.environ.get('REDISTOGO_URL'))
+			return redis.Redis(host=url.hostname, port=url.port, password=url.password)
+		else:
+			return redis.Redis(host=options.redis_host, port=options.redis_port, db=options.redis_db)
 
 # the main page
 class MainHandler(BaseHandler):
