98edf4f7d9dae821845afc55d6bb868bde2ce3bd
Mike Dory <mike@dory.me>
2011-11-25 17:22:02 -0500

    fixed recursive parsing

--- dictionary.py
+++ dictionary.py
@@ -12,12 +12,14 @@ def define_word(word):
 		body = response.body
 		body = body[2:-10]
 		lookup = ast.literal_eval(body)
+		counter = 0
 		if lookup.has_key("webDefinitions"):
 			definitions = lookup["webDefinitions"][0]["entries"]
 			for definition in definitions:	
 				if definition["type"] == "meaning":
-					string = urllib.unquote(definition['terms'][0]['text'])
+					string = urllib.unquote(definition['terms'][counter]['text'])
 					defined_words.append(string)
+					++counter
 				else:
 					return "=("
 			return defined_words
