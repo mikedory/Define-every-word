f589c57e9c44b5e88c20fc91aed146a859aca854
Mike Dory <mike@dory.me>
2011-12-03 14:57:41 -0500

    tweeting, I think?

--- tweet_grabber.py
+++ tweet_grabber.py
@@ -19,7 +19,7 @@ def set_db_defaults(db):
 	return checks
 
 def grab_twitter_updates():
-	from twitter import Twitter, NoAuth, OAuth, read_token_file
+	from twitter import Twitter, NoAuth
 	# twitter
 	noauth = NoAuth()
 	twitter_na = Twitter(domain='api.twitter.com', auth=noauth, api_version='1')
@@ -42,6 +42,15 @@ def grab_all_the_things():
 	print ("Last update id: %s" % lastUpdate["id"])
 	print ("Last update text: %s" % lastUpdate["text"])
 
+	'''
+	This should look like:
+
+	Last update created_at: Sat Nov 26 16:30:03 +0000 2011
+	Last update timestamp: 1322343003.0
+	Last update id: 140467359902203906
+	Last update text: optioning
+	'''
+
 
 	datters = db.get("tweets:%s" % lastUpdate["id"])
 	if datters:
@@ -55,3 +64,45 @@ def grab_all_the_things():
 
 if __name__ == "__main__":
 	grab_all_the_things()
+
+def send_tweet(word):
+	from twitter import Twitter, OAuth
+
+	if os.environ.has_key('TWITTER_APP_CONSUMER_KEY'):
+		# heroku-styles
+		consumer_key = urlparse(os.environ.get('TWITTER_APP_CONSUMER_KEY'))
+		consumer_secret = urlparse(os.environ.get('TWITTER_APP_CONSUMER_SECRET'))
+		oauth_token = urlparse(os.environ.get('TWITTER_USER_OAUTH_TOKEN'))
+		token_secret = urlparse(os.environ.get('TWITTER_USER_TOKEN_SECRET'))
+
+	else:
+		# local use, probably
+		from tornado.options import define, options
+		define("consumer_key", default=None, help="twitter app consumer key", type=str)
+		define("consumer_secret", default=None, help="twitter app consumer secret", type=str)
+		tornado.options.parse_command_line()
+
+		# set them credentials
+		oauth = OAuth(oauth_token, token_secret, consumer_key, consumer_secret)
+
+		# intialize things
+		twitter = Twitter(domain='api.twitter.com',
+		                  auth=oauth,
+		                  api_version='1')
+
+		# something about url shortening here, I guess?
+		# fancyShortner.shorten()
+
+		# Souljaboytellem!
+		tweet_string = ('The new @everyword "%s" means: ' % word)
+		twitter.statuses.update(status=tweet_string)
+		time.sleep(2)
+		print "tweeted!"
+
+	    # this is where I should probably make sure it worked, or re-loop?
+		time.sleep(2)
+		recent = grab_twitter_updates()
+		print recent[0]['text']
+		print isinstance(recent.rate_limit_remaining, int)
+		print isinstance(recent.rate_limit_reset, int)
+		print "tweet successful!"
