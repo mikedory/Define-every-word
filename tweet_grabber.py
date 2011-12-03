ae8ceaf115b48fe8350b45cb58c865ef3adcaddc
Mike Dory <mike@dory.me>
2011-12-03 15:10:38 -0500

    moving tweeting into its own file

--- tweet_grabber.py
+++ tweet_grabber.py
@@ -5,6 +5,8 @@ import datetime, time
 import redis
 import json
 
+import tweet_sender
+
 def get_redis_conn():
 	if os.environ.has_key('REDISTOGO_URL'):
 		url = urlparse(os.environ.get('REDISTOGO_URL'))
@@ -51,12 +53,10 @@ def grab_all_the_things():
 	Last update text: optioning
 	'''
 
-
 	datters = db.get("tweets:%s" % lastUpdate["id"])
 	if datters:
 		# eh, old tweet
 		print("Same tweet. Carry on.")
-		send_tweet(lastUpdate["text"])
 	else:
 		# it is a new tweet!
 		print("NEW TWEET!")
@@ -68,49 +68,8 @@ def grab_all_the_things():
 		print("Tweet saved at %s" % datetime.datetime.now())
 
 		# tweet the tweet!
-		send_tweet(lastUpdate["text"])
-
-def send_tweet(word):
-	from twitter import Twitter, OAuth
-
-	if os.environ.has_key('TWITTER_APP_CONSUMER_KEY'):
-		# heroku-styles
-		consumer_key = urlparse(os.environ.get('TWITTER_APP_CONSUMER_KEY'))
-		consumer_secret = urlparse(os.environ.get('TWITTER_APP_CONSUMER_SECRET'))
-		oauth_token = urlparse(os.environ.get('TWITTER_USER_OAUTH_TOKEN'))
-		token_secret = urlparse(os.environ.get('TWITTER_USER_TOKEN_SECRET'))
-
-	else:
-		# local use, probably
-		from tornado.options import define, options
-		define("consumer_key", default=None, help="twitter app consumer key", type=str)
-		define("consumer_secret", default=None, help="twitter app consumer secret", type=str)
-		tornado.options.parse_command_line()
-
-		# set them credentials
-		oauth = OAuth(oauth_token, token_secret, consumer_key, consumer_secret)
-
-		# intialize things
-		twitter = Twitter(domain='api.twitter.com',
-		                  auth=oauth,
-		                  api_version='1')
-
-		# something about url shortening here, I guess?
-		# fancyShortner.shorten()
-
-		# Souljaboytellem!
-		tweet_string = ('The new @everyword "%s" means: ' % word)
-		twitter.statuses.update(status=tweet_string)
-		time.sleep(2)
-		print "tweeted!"
-
-	    # this is where I should probably make sure it worked, or re-loop?
-		time.sleep(2)
-		recent = grab_twitter_updates()
-		print recent[0]['text']
-		print isinstance(recent.rate_limit_remaining, int)
-		print isinstance(recent.rate_limit_reset, int)
-		print "tweet successful!"
-
+		tweet_attempt = tweet_sender.send_tweet(lastUpdate["text"])
+		print tweet_attempt
+		
 if __name__ == "__main__":
 	grab_all_the_things()
