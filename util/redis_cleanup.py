b4e88ea6f7f09b4aa5551eb89bb9e932486171d2
Mike Dory <mdory@kbsp.com>
2013-06-30 17:44:59 -0400

    more pep-8 fixes

--- util/redis_cleanup.py
+++ util/redis_cleanup.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python
-import os.path, os, sys
-from urlparse import urlparse
-import tornado
-import redis
-
-# snag the login and all that
 import configs
 
-''' 
+'''
 this is kind of stupid to have in the first palce
 but it's basically required because i implemented keys wrong
 woooo
 '''
 
+
 # grab a list of all keys, and delete all over 99
 def trim_redis_keys():
-	db = configs.get_redis_conn()
-	keys = db.keys('*')
+    db = configs.get_redis_conn()
+    keys = db.keys('*')
 
-	print "I hear tell it's time to clean out some keys"
+    print "I hear tell it's time to clean out some keys"
 
-	print "key length: %s" % len(keys)
+    print "key length: %s" % len(keys)
 
-	if (len(keys) > 99):
-		keys.sort()
-		keys.reverse()
-		keys_to_delete = keys[99:len(keys)]
-		for key in keys_to_delete:
-			if key is not 'checks':
-				print ('deleting key: %s' % key)
-				db.delete(key)
-	else:
-		print "all good here. as you were."
+    if (len(keys) > 99):
+        keys.sort()
+        keys.reverse()
+        keys_to_delete = keys[99:len(keys)]
+        for key in keys_to_delete:
+            if key is not 'checks':
+                print ('deleting key: %s' % key)
+                db.delete(key)
+    else:
+        print "all good here. as you were."
 
 if __name__ == "__main__":
-	trim_redis_keys()
+    trim_redis_keys()
--- util/tweet_grabber.py
+++ util/tweet_grabber.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-import os.path, os, sys
-from urlparse import urlparse
-import datetime, time
-import redis
+import datetime
+import time
 import json
 
 import configs
@@ -11,106 +9,106 @@ import tweet_shortener
 import word_grabber
 import redis_cleanup
 
+
 def grab_twitter_updates(tweet_id=None):
-	# tweetorz
-	from twitter import Twitter, OAuth
-	vars = configs.get_twitter_vars()
-	oauth = OAuth(vars["oauth_token"], vars["token_secret"], vars["consumer_key"], vars["consumer_secret"])
+    # tweetorz
+    from twitter import Twitter, OAuth
+    vars = configs.get_twitter_vars()
+    oauth = OAuth(vars["oauth_token"], vars["token_secret"], vars["consumer_key"], vars["consumer_secret"])
 
-	# intialize things
-	twitter = Twitter(domain='api.twitter.com',
-					  auth=oauth,
-					  api_version='1')
-	if tweet_id is not None:
-		return twitter.statuses.show(id=tweet_id)
-	else:
-		return twitter.statuses.user_timeline(screen_name="everyword")
-	
+    # intialize things
+    twitter = Twitter(domain='api.twitter.com',
+                      auth=oauth,
+                      api_version='1.1')
+    if tweet_id is not None:
+        return twitter.statuses.show(id=tweet_id)
+    else:
+        return twitter.statuses.user_timeline(screen_name="everyword")
+
+
 def grab_all_the_things():
-	# oh hai redis
-	db = configs.get_redis_conn()
-	defaults = configs.set_db_defaults(db)
+    # oh hai redis
+    db = configs.get_redis_conn()
 
-	print ("--------------------------------------")
+    print ("--------------------------------------")
 
-	# teh twitter
-	updates = grab_twitter_updates()
-	lastUpdate = updates[0]
-	timestampstring = lastUpdate["created_at"] + ' UTC'
-	timestamp = time.mktime(time.strptime(timestampstring,  '%a  %b %d %H:%M:%S +0000 %Y %Z'))
-	lastUpdate["timestamp"] = timestamp
+    # teh twitter
+    updates = grab_twitter_updates()
+    lastUpdate = updates[0]
+    timestampstring = lastUpdate["created_at"] + ' UTC'
+    timestamp = time.mktime(time.strptime(timestampstring,  '%a  %b %d %H:%M:%S +0000 %Y %Z'))
+    lastUpdate["timestamp"] = timestamp
 
-	print ("Last update created_at: %s" % lastUpdate["created_at"])
-	print ("Last update timestamp: %s" % lastUpdate["timestamp"])
-	print ("Last update id: %s" % lastUpdate["id"])
-	print ("Last update text: %s" % lastUpdate["text"])
+    print ("Last update created_at: %s" % lastUpdate["created_at"])
+    print ("Last update timestamp: %s" % lastUpdate["timestamp"])
+    print ("Last update id: %s" % lastUpdate["id"])
+    print ("Last update text: %s" % lastUpdate["text"])
 
-	'''
-	This should look like:
+    '''
+    This should look like:
 
-	Last update created_at: Sat Nov 26 16:30:03 +0000 2011
-	Last update timestamp: 1322343003.0
-	Last update id: 140467359902203906
-	Last update text: optioning
-	'''
+    Last update created_at: Sat Nov 26 16:30:03 +0000 2011
+    Last update timestamp: 1322343003.0
+    Last update id: 140467359902203906
+    Last update text: optioning
+    '''
 
-	datters = db.get("tweets:%s" % lastUpdate["id"])
-	if datters:
-		# eh, old tweet
-		print("Same tweet. Carry on.")
-	else:
-		# it is a new tweet!
-		print("NEW TWEET!")
-		
-		saved = save_tweet(lastUpdate)
-		print 'saved!'
+    datters = db.get("tweets:%s" % lastUpdate["id"])
+    if datters:
+        # eh, old tweet
+        print("Same tweet. Carry on.")
+    else:
+        # it is a new tweet!
+        print("NEW TWEET!")
 
-		tweeted = tweet_tweet(lastUpdate)
-		print 'tweeted!'
+        saved = save_tweet(lastUpdate)
+        print 'saved!'
 
-		redis_cleanup.trim_redis_keys()
-		print 'cleaned!'
+        tweeted = tweet_tweet(lastUpdate)
+        print 'tweeted!'
 
-		return tweeted,saved
+        redis_cleanup.trim_redis_keys()
+        print 'cleaned!'
+
+        return tweeted, saved
 
 
-def save_tweet(lastUpdate):		
-	# set up the DB
-	db = configs.get_redis_conn()
-	defaults = configs.set_db_defaults(db)
+def save_tweet(lastUpdate):
+    # set up the DB
+    db = configs.get_redis_conn()
 
-	# save the tweet!
-	db.set(("tweets:%s" % lastUpdate["id"]), json.dumps(lastUpdate))
-	db.lpush("tweets:tweet_ids", lastUpdate["id"])
-	return "Tweet \"%s\"saved at %s" % (lastUpdate["text"], datetime.datetime.now())
+    # save the tweet!
+    db.set(("tweets:%s" % lastUpdate["id"]), json.dumps(lastUpdate))
+    db.lpush("tweets:tweet_ids", lastUpdate["id"])
+    return "Tweet \"%s\"saved at %s" % (lastUpdate["text"], datetime.datetime.now())
+
 
 def tweet_tweet(lastUpdate):
-	# work out the definition
-	print("defining %s..." % lastUpdate['text'])
+    # work out the definition
+    print("defining %s..." % lastUpdate['text'])
 
-	# define things
-	definition_data = word_grabber.define_word(lastUpdate['text'])
+    # define things
+    definition_data = word_grabber.define_word(lastUpdate['text'])
 
-	if definition_data is not None:
+    if definition_data is not None:
 
-		defintition = definition_data['definitions'][0]
-		word = lastUpdate['text']
-		short_def = tweet_shortener.shorten_definition(lastUpdate['text'], definition_data['definitions'][0])
+        word = lastUpdate['text']
+        short_def = tweet_shortener.shorten_definition(lastUpdate['text'], definition_data['definitions'][0])
 
-		# set up the link
-		link = 'http://defineeveryword.heroku.com/%s' % lastUpdate["id"]
+        # set up the link
+        link = 'http://defineeveryword.heroku.com/%s' % lastUpdate["id"]
 
-		# set up the tweet string
-		tweet_string = "%s: %s %s" % (lastUpdate["text"], short_def, link)
-		print "tweeting: %s" % tweet_string
+        # set up the tweet string
+        tweet_string = "%s: %s %s" % (lastUpdate["text"], short_def, link)
+        print "tweeting: %s" % tweet_string
 
-		# get the vars
-		vars = configs.get_twitter_vars()
-		tweet_attempt = tweet_sender.send_tweet(word, tweet_string, vars["consumer_key"], vars["consumer_secret"], vars["oauth_token"], vars["token_secret"])
-		return tweet_attempt	
-	else:
-		return "nothing to tweet =("
+        # get the vars
+        vars = configs.get_twitter_vars()
+        tweet_attempt = tweet_sender.send_tweet(word, tweet_string, vars["consumer_key"], vars["consumer_secret"], vars["oauth_token"], vars["token_secret"])
+        return tweet_attempt
+    else:
+        return "nothing to tweet =("
 
-		
+
 if __name__ == "__main__":
-	grab_all_the_things()
+    grab_all_the_things()
